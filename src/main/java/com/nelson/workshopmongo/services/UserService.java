9f341ae47ddc52c8c23ae016fb584123f2b135c4
NelsonFSP <nelson.santosdepaula@yahoo.com.br>
2020-07-12 14:25:53 -0300

    User insert with POST

--- src/main/java/com/nelson/workshopmongo/services/UserService.java
+++ src/main/java/com/nelson/workshopmongo/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.nelson.workshopmongo.domain.User;
 import com.nelson.workshopmongo.repository.UserRepository;
 import com.nelson.workshopmongo.services.exception.ObjectNotFoundException;
+import com.nelson.workshopmongo.sto.UserDTO;
 
 @Service
 public class UserService {
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
